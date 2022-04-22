9dd25e932bc93fa41c8d3fcf08656a7567632823
gunnar <gunnark@animocabrands.com>
2022-04-22 11:55:12 +0800

    Add 'approve' function implementation to ERC721Storage

--- contracts/token/ERC721/libraries/ERC721Storage.sol
+++ contracts/token/ERC721/libraries/ERC721Storage.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.8;
 
 import {IERC721} from "./../interfaces/IERC721.sol";
-import {IERC721Events} from "./../interfaces/IERC721Events.sol";
 import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 import {StorageVersion} from "./../../../proxy/libraries/StorageVersion.sol";
 import {InterfaceDetectionStorage} from "./../../../introspection/libraries/InterfaceDetectionStorage.sol";
@@ -21,6 +20,9 @@ library ERC721Storage {
 
     bytes32 public constant ERC721_STORAGE_POSITION = bytes32(uint256(keccak256("animoca.token.ERC721.ERC721.storage")) - 1);
     bytes32 public constant ERC721_VERSION_SLOT = bytes32(uint256(keccak256("animoca.token.ERC721.ERC721.version")) - 1);
+    uint256 internal constant _APPROVAL_BIT_TOKEN_OWNER_ = 1 << 160;
+
+    event Approval(address indexed _owner, address indexed _approved, uint256 indexed _tokenId);
 
     /// @notice Initialises the storage.
     /// @notice Sets the ERC721 storage version to `1`.
@@ -29,6 +31,43 @@ library ERC721Storage {
     function init() internal {
         StorageVersion.setVersion(ERC721_VERSION_SLOT, 1);
         InterfaceDetectionStorage.layout().setSupportedInterface(type(IERC721).interfaceId, true);
+    }
+
+    function approve(
+        Layout storage s, 
+        address sender, 
+        address to, 
+        uint256 tokenId
+    ) internal {
+        uint256 owner = s.owners[tokenId];
+        require(owner != 0, "ERC721: non-existing NFT");
+        address ownerAddress = address(uint160(owner));
+        require(to != ownerAddress, "ERC721: self-approval");
+        require(_isOperatable(s, ownerAddress, sender), "ERC721: non-approved sender");
+        if (to == address(0)) {
+            if (owner & _APPROVAL_BIT_TOKEN_OWNER_ != 0) {
+                // remove the approval bit if it is present
+                s.owners[tokenId] = uint256(uint160(ownerAddress));
+            }
+        } else {
+            uint256 ownerWithApprovalBit = owner | _APPROVAL_BIT_TOKEN_OWNER_;
+            if (owner != ownerWithApprovalBit) {
+                // add the approval bit if it is not present
+                s.owners[tokenId] = ownerWithApprovalBit;
+            }
+            s.nftApprovals[tokenId] = to;
+        }
+        emit Approval(ownerAddress, to, tokenId);
+    }
+
+        /**
+     * Returns whether `sender` is authorised to make a transfer on behalf of `from`.
+     * @param from The address to check operatibility upon.
+     * @param sender The sender address.
+     * @return True if sender is `from` or an operator for `from`, false otherwise.
+     */
+    function _isOperatable(Layout storage s, address from, address sender) internal view returns (bool) {
+        return (from == sender) || s.operators[from][sender];
     }
 
     function layout() internal pure returns (Layout storage s) {
