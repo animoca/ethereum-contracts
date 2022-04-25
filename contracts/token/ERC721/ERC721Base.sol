402936e94bf32bf78837eb27e8b1d6ef9bb9a7f8
gunnar <gunnark@animocabrands.com>
2022-04-25 13:59:58 +0800

    Add ERC721Base implementation

---
+++ contracts/token/ERC721/ERC721Base.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import {IERC721} from "./interfaces/IERC721.sol";
+import {IERC721Events} from "./interfaces/IERC721Events.sol";
+import {IERC721Metadata} from "./interfaces/IERC721Metadata.sol";
+import {ERC721Storage} from "./libraries/ERC721Storage.sol";
+import {ERC721MetadataStorage} from "./libraries/ERC721MetadataStorage.sol";
+import {Context} from "@openzeppelin/contracts/utils/Context.sol";
+
+/// @title ERC721 Non-Fungible Token Standard (proxiable version).
+/// @dev This contract is to be used via inheritance in a proxied implementation.
+/// @dev `ERC721Storage.init` should be called during contract initialization.
+abstract contract ERC721Base is Context, IERC721, IERC721Events {
+    using ERC721Storage for ERC721Storage.Layout;
+    using ERC721MetadataStorage for ERC721MetadataStorage.Layout;
+
+    function name() public view virtual returns (string memory) {
+        return ERC721MetadataStorage.layout().name;
+    }
+
+    function symbol() public view virtual returns (string memory) {
+        return ERC721MetadataStorage.layout().symbol;
+    }
+
+    /// @inheritdoc IERC721
+    function balanceOf(address owner) public view virtual override returns (uint256) {
+        return ERC721Storage.layout().nftBalances[owner];
+    }
+
+    /// @inheritdoc IERC721
+    function ownerOf(uint256 tokenId) public view virtual override returns (address) {
+        return address(uint160(ERC721Storage.layout().owners[tokenId]));
+    }
+
+    /// @inheritdoc IERC721
+     function approve(address to, uint256 tokenId) public virtual override {
+         ERC721Storage.layout().approve(_msgSender(), to, tokenId);
+     }
+
+     /// @inheritdoc IERC721
+     function setApprovalForAll(address operator, bool _approved) public virtual override {
+         ERC721Storage.layout().setApprovalForAll(_msgSender(), operator, _approved);
+     }
+
+    /// @inheritdoc IERC721
+     function transferFrom(address from, address to, uint256 tokenId) public virtual override {
+         ERC721Storage.layout().transferFrom(_msgSender(), from, to, tokenId);
+     }
+
+    /// @inheritdoc IERC721
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory _data) public virtual override {
+        ERC721Storage.layout().safeTransferFrom(_msgSender(), from, to, tokenId, _data);
+    }
+
+    /// @inheritdoc IERC721
+    function safeTransferFrom(address from, address to, uint256 tokenId) public virtual override {
+        ERC721Storage.layout().safeTransferFrom(_msgSender(), from, to, tokenId);
+    }
+
+}
