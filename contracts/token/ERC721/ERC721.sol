7de6a7c8576988affc148b18342a09a944642bc4
gunnar <gunnark@animocabrands.com>
2022-05-03 12:02:17 +0800

    Remove log statements from ERC721

--- contracts/token/ERC721/ERC721.sol
+++ contracts/token/ERC721/ERC721.sol
@@ -3,13 +3,11 @@ pragma solidity ^0.8.8;
 
 import {ERC721Base} from "./ERC721Base.sol";
 import {ERC721Storage} from "./libraries/ERC721Storage.sol";
-import "hardhat/console.sol";
 
 contract ERC721 is ERC721Base {
     using ERC721Storage for ERC721Storage.Layout;
 
     constructor() {
-        console.log("ERC721 constructor");
         ERC721Storage.layout().init();
     }
 
