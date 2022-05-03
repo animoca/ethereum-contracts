31de59f7cf0e55048ddd2f1a5753a09ff0721918
gunnar <gunnark@animocabrands.com>
2022-05-03 12:02:57 +0800

    Add ERC721Receiver and ERC721ReceiverMock

---
+++ contracts/mocks/token/ERC721/ERC721ReceiverMock.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import {ERC721} from "./../../../token/ERC721/ERC721.sol";
+import {ERC721Receiver} from "./../../../token/ERC721/ERC721Receiver.sol";
+import {IERC721Receiver} from "./../../../token/ERC721/interfaces/IERC721Receiver.sol";
+
+/**
+ * @title ERC721 Receiver Mock.
+ */
+contract ERC721ReceiverMock is ERC721Receiver {
+    event Received(address operator, address from, uint256 tokenId, bytes data, uint256 gas);
+
+    bool internal immutable _accept721;
+    address internal immutable _tokenAddress721;
+
+    constructor(bool accept721, address tokenAddress) ERC721Receiver() {
+        _accept721 = accept721;
+        _tokenAddress721 = tokenAddress;
+    }
+
+    //=================================================== ERC721Receiver ====================================================//
+
+    /// @inheritdoc IERC721Receiver
+    /// @dev reverts if the sender is not the supported ERC721 contract.
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes memory data
+    ) public virtual override returns (bytes4) {
+        require(msg.sender == _tokenAddress721, "ERC721Receiver: wrong token");
+        if (_accept721) {
+            emit Received(operator, from, tokenId, data, gasleft());
+            return _ERC721_RECEIVED;
+        } else {
+            return _ERC721_REJECTED;
+        }
+    }
+}
---
+++ contracts/token/ERC721/ERC721Receiver.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.13;
+
+import {IERC165} from "./../../introspection/interfaces/IERC165.sol";
+import {IERC721Receiver} from "./interfaces/IERC721Receiver.sol";
+
+/**
+ * @title ERC721 Safe Transfers Receiver Contract.
+ * @dev The function `onERC721Received(address,address,uint256,bytes)` needs to be implemented by a child contract.
+ */
+abstract contract ERC721Receiver is IERC165, IERC721Receiver {
+    bytes4 internal constant _ERC721_RECEIVED = type(IERC721Receiver).interfaceId;
+    bytes4 internal constant _ERC721_REJECTED = 0xffffffff;
+
+    //======================================================= ERC165 ========================================================//
+
+    /// @inheritdoc IERC165
+    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
+        return interfaceId == type(IERC165).interfaceId || interfaceId == type(IERC721Receiver).interfaceId;
+    }
+}
