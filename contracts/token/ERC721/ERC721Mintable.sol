70bf20c9cd417db77633c7e40650cfab113cbc5f
gunnar <gunnark@animocabrands.com>
2022-05-12 15:24:49 +0800

    Add AccessControlStorage to MintableBase

--- contracts/token/ERC721/ERC721Mintable.sol
+++ contracts/token/ERC721/ERC721Mintable.sol
@@ -1,6 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
+import {IERC721MintableOnce} from "./interfaces/IERC721MintableOnce.sol";
 import {ERC721MintableBase} from "./ERC721MintableBase.sol";
+import {AccessControl} from "./../../access/AccessControl.sol";
+import {InterfaceDetectionStorage} from "./../../introspection/libraries/InterfaceDetectionStorage.sol";
 
-abstract contract ERC721Mintable is ERC721MintableBase {}
+abstract contract ERC721Mintable is ERC721MintableBase {
+    using InterfaceDetectionStorage for InterfaceDetectionStorage.Layout;
+
+    /// @notice Marks the following ERC165 interface(s) as supported: ERC20MintableOnce.
+    constructor() {
+        InterfaceDetectionStorage.layout().setSupportedInterface(type(IERC721MintableOnce).interfaceId, true);
+
+    }
+}
--- contracts/token/ERC721/ERC721MintableBase.sol
+++ contracts/token/ERC721/ERC721MintableBase.sol
@@ -2,17 +2,22 @@
 pragma solidity ^0.8.8;
 
 import {IERC721Mintable} from "./interfaces/IERC721Mintable.sol";
+import {AccessControlStorage} from "./../../access/libraries/AccessControlStorage.sol";
 import {ERC721Storage} from "./libraries/ERC721Storage.sol";
 import {Context} from "@openzeppelin/contracts/utils/Context.sol";
 
 abstract contract ERC721MintableBase is Context, IERC721Mintable {
+    using AccessControlStorage for AccessControlStorage.Layout;
     using ERC721Storage for ERC721Storage.Layout;
+
+    bytes32 public constant MINTER_ROLE = "minter";
 
     function mint(address to, uint256 tokenId) external {
         ERC721Storage.layout().mint(_msgSender(), to, tokenId, "", false);
     }
 
     function batchMint(address to, uint256[] calldata tokenIds) external {
+        AccessControlStorage.layout().enforceHasRole(MINTER_ROLE, _msgSender());
         ERC721Storage.layout().batchMint(to, tokenIds);
     }
 
