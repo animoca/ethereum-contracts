18eff978b0b10126d7059bac8b9dce0a0d62e5b9
Nathan Sala <sala.nathan@gmail.com>
2022-05-17 18:16:30 +0900

    removed import-artifacts plugin, fixed plugins import sequence, added ERC721 and ERC1155 solidity interfaces, improved the logic of runBehaviorTests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,24 @@
 # Changelog
 
+## 0.0.5
+
+### Breaking changes
+
+- Removed HardHat plugin `import-artifacts` in favor of `hardhat-deploy`'s `external` configuration.
+
+### New features
+
+- Added more ERC721 interfaces.
+- Added the ERC1155 basic interfaces.
+
+### Bugfixes
+
+- Fix HardHat plugins import order to prevent `hardhat-ethers` to override functions provided by `hardhat-deploy-ethers`.
+
+### Improvements
+
+- `runBehaviorTests`: Immutable tests are run only if there is an `immutable` section. Diamond tests will run only if there is a `diamond` section.
+
 ## 0.0.4
 
 ### Improvements
---
+++ contracts/token/ERC721/interfaces/IERC721Receiver.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+/**
+ * @title ERC721 Non-Fungible Token Standard, Tokens Receiver.
+ * Interface for any contract that wants to support safeTransfers from ERC721 asset contracts.
+ * @dev See https://eips.ethereum.org/EIPS/eip-721
+ * @dev Note: The ERC-165 identifier for this interface is 0x150b7a02.
+ */
+interface IERC721Receiver {
+    /**
+     * Handles the receipt of an NFT.
+     * @dev The ERC721 smart contract calls this function on the recipient
+     *  after a {IERC721-safeTransferFrom}. This function MUST return the function selector,
+     *  otherwise the caller will revert the transaction. The selector to be
+     *  returned can be obtained as `this.onERC721Received.selector`. This
+     *  function MAY throw to revert and reject the transfer.
+     * @dev Note: the ERC721 contract address is always the message sender.
+     * @param operator The address which called `safeTransferFrom` function
+     * @param from The address which previously owned the token
+     * @param tokenId The NFT identifier which is being transferred
+     * @param data Additional data with no specified format
+     * @return bytes4 `bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"))`
+     */
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external returns (bytes4);
+}
