eabfcccc674d688e7bfc205165e5e185264cc96d
gunnar <gunnark@animocabrands.com>
2022-05-23 12:50:53 +0800

    Remove 'only' filter for ERC721 tests

--- test/contracts/token/ERC721/behaviors/ERC721.behavior.js
+++ test/contracts/token/ERC721/behaviors/ERC721.behavior.js
@@ -3,9 +3,8 @@ const { shouldBehaveLikeERC721Mintable } = require('./ERC721.mintable.behavior')
 const { shouldBehaveLikeERC721Burnable } = require('./ERC721.burnable.behavior');
 const { shouldBehaveLikeERC721Metadata } = require('./ERC721.metadata.behavior');
 
-
 function behavesLikeERC721(implementation) {
-    describe.only('like an ERC721', function() { // TODO: remove .only
+    describe('like an ERC721', function() {
         if (implementation.interfaces.ERC721) {
             shouldBehaveLikeERC721Standard(implementation);
         }
