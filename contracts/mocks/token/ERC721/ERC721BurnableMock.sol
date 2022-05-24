7783aa9d5e9c13791606fb21aaaacc60ed0ee08e
gunnar <gunnark@animocabrands.com>
2022-05-24 11:20:59 +0800

    Fix warning with unused parameters for BurnableMock

--- contracts/mocks/token/ERC721/ERC721BurnableMock.sol
+++ contracts/mocks/token/ERC721/ERC721BurnableMock.sol
@@ -2,15 +2,15 @@
 pragma solidity 0.8.13;
 
 import {ERC721} from "./../../../token/ERC721/ERC721.sol";
-
 import {ERC721Burnable} from "./../../../token/ERC721/ERC721Burnable.sol";
 import {ERC721Mintable} from "./../../../token/ERC721/ERC721Mintable.sol";
+import {ERC721TokenMetadataWithBaseURI} from "./../../../token/ERC721/ERC721TokenMetadataWithBaseURI.sol";
 
 /// @title ERC721BurnableMock
-contract ERC721BurnableMock is ERC721, ERC721Mintable, ERC721Burnable {
+contract ERC721BurnableMock is ERC721, ERC721Mintable, ERC721Burnable, ERC721TokenMetadataWithBaseURI {
     constructor(
         string memory name_,
         string memory symbol_,
         string memory tokenURI_
-    ) ERC721Burnable() {}
+    ) ERC721TokenMetadataWithBaseURI(name_, symbol_, tokenURI_) {}
 }
--- contracts/mocks/token/ERC721/ERC721MintableOnceMock.sol
+++ contracts/mocks/token/ERC721/ERC721MintableOnceMock.sol
@@ -4,13 +4,15 @@ pragma solidity 0.8.13;
 import {ERC721} from "./../../../token/ERC721/ERC721.sol";
 import {ERC721MintableOnce} from "./../../../token/ERC721/ERC721MintableOnce.sol";
 import {ERC721Burnable} from "./../../../token/ERC721/ERC721Burnable.sol";
+import {ERC721TokenMetadataWithBaseURI} from "./../../../token/ERC721/ERC721TokenMetadataWithBaseURI.sol";
 import {Ownable} from "./../../../access/Ownable.sol";
 
+
 /// @title ERC721MintableOnceFacetMock
-contract ERC721MintableOnceMock is ERC721, ERC721MintableOnce, ERC721Burnable {
+contract ERC721MintableOnceMock is ERC721, ERC721MintableOnce, ERC721Burnable, ERC721TokenMetadataWithBaseURI {
     constructor(
         string memory name_,
         string memory symbol_,
         string memory tokenURI_
-    ) ERC721MintableOnce() ERC721Burnable() Ownable(msg.sender) {}
+    ) ERC721TokenMetadataWithBaseURI(name_, symbol_, tokenURI_) ERC721MintableOnce() ERC721Burnable() Ownable(msg.sender) {}
 }
