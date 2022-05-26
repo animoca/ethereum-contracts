250992511c5ab7831a7a669ecf52a8795c555370
gunnar <gunnark@animocabrands.com>
2022-05-26 10:03:27 +0800

    Re-enable ERCMintableOnceMock tests

--- test/contracts/token/ERC721/ERC721MintableOnceMock.test.js
+++ test/contracts/token/ERC721/ERC721MintableOnceMock.test.js
@@ -1,115 +1,114 @@
-const { behavesLikeERC721 } = require('./behaviors/ERC721.behavior');
-const { getDeployerAddress, getForwarderRegistryAddress, runBehaviorTests } = require('../../../helpers/run');
+const {behavesLikeERC721} = require('./behaviors/ERC721.behavior');
+const {getDeployerAddress, getForwarderRegistryAddress, runBehaviorTests} = require('../../../helpers/run');
 
 const name = 'ERC721 MintableOnce Mock';
 const symbol = 'E721MINTABLEONCE';
 const tokenURI = 'uri';
 
 const config = {
-    immutable: {
-        name: 'ERC721MintableOnceMock',
-        ctorArguments: ['name', 'symbol', 'tokenURI'],
+  immutable: {
+    name: 'ERC721MintableOnceMock',
+    ctorArguments: ['name', 'symbol', 'tokenURI'],
+    metaTxSupport: true,
+  },
+  diamond: {
+    facets: [
+      {name: 'ProxyAdminFacetMock', ctorArguments: ['forwarderRegistry'], init: {method: 'initProxyAdminStorage', arguments: ['initialAdmin']}},
+      {name: 'DiamondCutFacet', ctorArguments: ['forwarderRegistry'], init: {method: 'initDiamondCutStorage'}},
+      {name: 'ERC165Facet', ctorArguments: ['forwarderRegistry'], init: {method: 'initInterfaceDetectionStorage'}},
+      {name: 'OwnableFacet', ctorArguments: ['forwarderRegistry'], init: {method: 'initOwnershipStorage', arguments: ['initialOwner']}},
+      {name: 'AccessControlFacet', ctorArguments: ['forwarderRegistry']},
+      {
+        name: 'ERC721FacetMock',
+        ctorArguments: ['forwarderRegistry'],
+        init: {
+          method: 'initERC721Storage',
+          arguments: [],
+          adminProtected: true,
+          versionProtected: true,
+        },
         metaTxSupport: true,
-    },
-    diamond: {
-        facets: [
-            { name: 'ProxyAdminFacetMock', ctorArguments: ['forwarderRegistry'], init: { method: 'initProxyAdminStorage', arguments: ['initialAdmin'] } },
-            { name: 'DiamondCutFacet', ctorArguments: ['forwarderRegistry'], init: { method: 'initDiamondCutStorage' } },
-            { name: 'ERC165Facet', ctorArguments: ['forwarderRegistry'], init: { method: 'initInterfaceDetectionStorage' } },
-            { name: 'OwnableFacet', ctorArguments: ['forwarderRegistry'], init: { method: 'initOwnershipStorage', arguments: ['initialOwner'] } },
-            { name: 'AccessControlFacet', ctorArguments: ['forwarderRegistry'] },
-            {
-                name: 'ERC721FacetMock',
-                ctorArguments: ['forwarderRegistry'],
-                init: {
-                    method: 'initERC721Storage',
-                    arguments: [],
-                    adminProtected: true,
-                    versionProtected: true,
-                },
-                metaTxSupport: true,
-            },
-            {
-                name: 'ERC721MintableOnceFacetMock',
-                ctorArguments: ['forwarderRegistry'],
-                init: {
-                    method: 'initERC721MintableOnceStorage',
-                    arguments: [],
-                    adminProtected: true,
-                    versionProtected: false,
-                },
-                metaTxSupport: true,
-            },
-            {
-                name: 'ERC721BurnableFacetMock',
-                ctorArguments: ['forwarderRegistry'],
-                init: {
-                    method: 'initERC721BurnableStorage',
-                    arguments: [],
-                    adminProtected: true,
-                    versionProtected: false,
-                },
-                metaTxSupport: true,
-            },
-        ],
-    },
-    defaultArguments: {
-        forwarderRegistry: getForwarderRegistryAddress,
-        initialAdmin: getDeployerAddress,
-        initialOwner: getDeployerAddress,
-        name,
-        symbol,
-        tokenURI,
-    },
+      },
+      {
+        name: 'ERC721MintableOnceFacetMock',
+        ctorArguments: ['forwarderRegistry'],
+        init: {
+          method: 'initERC721MintableOnceStorage',
+          arguments: [],
+          adminProtected: true,
+          versionProtected: false,
+        },
+        metaTxSupport: true,
+      },
+      {
+        name: 'ERC721BurnableFacetMock',
+        ctorArguments: ['forwarderRegistry'],
+        init: {
+          method: 'initERC721BurnableStorage',
+          arguments: [],
+          adminProtected: true,
+          versionProtected: false,
+        },
+        metaTxSupport: true,
+      },
+    ],
+  },
+  defaultArguments: {
+    forwarderRegistry: getForwarderRegistryAddress,
+    initialAdmin: getDeployerAddress,
+    initialOwner: getDeployerAddress,
+    name,
+    symbol,
+    tokenURI,
+  },
 };
 
 const includeDiamondTest = true;
 runBehaviorTests(
-    'Mintable Once ERC721',
-    config,
-    function(deployFn) {
-        const implementation = {
-            name,
-            symbol,
-            tokenURI,
-            revertMessages: {
-                NonApproved: 'ERC721: non-approved sender',
-                SelfApproval: 'ERC721: self-approval',
-                SelfApprovalForAll: 'ERC721: self-approval',
-                ZeroAddress: 'ERC721: zero address',
-                TransferToZero: 'ERC721: transfer to zero',
-                MintToZero: 'ERC721: mint to zero',
-                TransferRejected: 'ERC721: transfer refused',
-                NonExistingNFT: 'ERC721: non-existing NFT',
-                NonOwnedNFT: 'ERC721: non-owned NFT',
-                ExistingOrBurntNFT: 'ERC721: existing/burnt NFT',
-                BurntNFT: 'ERC721: burnt NFT',
+  'Mintable Once ERC721',
+  config,
+  function (deployFn) {
+    const implementation = {
+      name,
+      symbol,
+      tokenURI,
+      revertMessages: {
+        NonApproved: 'ERC721: non-approved sender',
+        SelfApproval: 'ERC721: self-approval',
+        SelfApprovalForAll: 'ERC721: self-approval',
+        ZeroAddress: 'ERC721: zero address',
+        TransferToZero: 'ERC721: transfer to zero',
+        MintToZero: 'ERC721: mint to zero',
+        TransferRejected: 'ERC721: transfer refused',
+        NonExistingNFT: 'ERC721: non-existing NFT',
+        NonOwnedNFT: 'ERC721: non-owned NFT',
+        ExistingOrBurntNFT: 'ERC721: existing/burnt NFT',
+        BurntNFT: 'ERC721: burnt NFT',
 
-                // Admin
-                NotMinter: "AccessControl: missing 'minter' role",
-                NotContractOwner: 'Ownable: not the owner',
-            },
-            features: {
-                ERC721MintableOnce: true,
-            },
-            interfaces: {
-                ERC721Mintable: true, // MintableOnce should pass tests for Mintable
-                ERC721Burnable: true,
-            },
-            methods: {
-                'batchTransferFrom(address,address,uint256[])': async function(contract, from, to, ids, signer) {
-                    return contract.connect(signer).batchTransferFrom(from, to, ids);
-                }
-            },
-            deploy: async function(name, symbol, tokenURI, deployer) {
-                const contract = await deployFn({ name, symbol, tokenURI });
-                await contract.grantRole(await contract.MINTER_ROLE(), deployer.address);
-                return contract;
-            },
-        };
-        if (false) {
-            behavesLikeERC721(implementation);
-        }
-    },
-    includeDiamondTest
+        // Admin
+        NotMinter: "AccessControl: missing 'minter' role",
+        NotContractOwner: 'Ownable: not the owner',
+      },
+      features: {
+        ERC721MintableOnce: true,
+      },
+      interfaces: {
+        ERC721Mintable: true, // MintableOnce should pass tests for Mintable
+        ERC721Burnable: true,
+      },
+      methods: {
+        'batchTransferFrom(address,address,uint256[])': async function (contract, from, to, ids, signer) {
+          return contract.connect(signer).batchTransferFrom(from, to, ids);
+        },
+      },
+      deploy: async function (name, symbol, tokenURI, deployer) {
+        const contract = await deployFn({name, symbol, tokenURI});
+        await contract.grantRole(await contract.MINTER_ROLE(), deployer.address);
+        return contract;
+      },
+    };
+
+    behavesLikeERC721(implementation);
+  },
+  includeDiamondTest
 );
