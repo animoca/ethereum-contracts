04fa0ea3bc4f64db40a5f12629e16eabc699f328
gunnar <gunnark@animocabrands.com>
2022-05-27 11:07:52 +0800

    Add ERC721BurnableMock and ERC721BurnableMock.test

--- contracts/mocks/token/ERC721/ERC721BurnableMock.sol
+++ contracts/mocks/token/ERC721/ERC721BurnableMock.sol
@@ -1,20 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
+import {IForwarderRegistry} from "./../../../metatx/interfaces/IForwarderRegistry.sol";
 import {ERC721} from "./../../../token/ERC721/ERC721.sol";
 import {ERC721Burnable} from "./../../../token/ERC721/ERC721Burnable.sol";
 import {ERC721Mintable} from "./../../../token/ERC721/ERC721Mintable.sol";
+import {ERC721Mock} from "./ERC721Mock.sol";
 import {ERC721TokenMetadataWithBaseURI} from "./../../../token/ERC721/ERC721TokenMetadataWithBaseURI.sol";
+import {ForwarderRegistryContextBase} from "./../../../metatx/ForwarderRegistryContextBase.sol";
+import {Context} from "@openzeppelin/contracts/utils/Context.sol";
 
 /// @title ERC721BurnableMock
-contract ERC721BurnableMock is ERC721, ERC721Mintable, ERC721Burnable, ERC721TokenMetadataWithBaseURI {
+contract ERC721BurnableMock is ERC721Mock {
     constructor(
         string memory name_,
         string memory symbol_,
-        string memory tokenURI_
-    ) ERC721TokenMetadataWithBaseURI(name_, symbol_, tokenURI_) {}
+        string memory tokenURI_,
+        IForwarderRegistry forwarderRegistry
+    ) ERC721Mock(name_, symbol_, tokenURI_, forwarderRegistry) {}
 
-    function __msgData() external view returns (bytes calldata) {
-        return _msgData();
-    }
 }
