a828f409fe913026470f92cc81fa436e0989275c
gunnar <gunnark@animocabrands.com>
2022-05-27 15:33:22 +0800

    Format interface comments with single line NatSpec comments

--- contracts/token/ERC721/interfaces/IERC721Burnable.sol
+++ contracts/token/ERC721/interfaces/IERC721Burnable.sol
@@ -1,29 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-/**
- * @title ERC721 Non-Fungible Token Standard, optional extension: Burnable.
- * @dev See https://eips.ethereum.org/EIPS/eip-721
- * @dev Note: The ERC-165 identifier for this interface is 0x8b8b4ef5.
- */
+/// @title ERC721 Non-Fungible Token Standard, optional extension: Burnable.
+/// @dev See https://eips.ethereum.org/EIPS/eip-721
+/// @dev Note: The ERC-165 identifier for this interface is 0x8b8b4ef5.
 interface IERC721Burnable {
-    /**
-     * Burns a token.
-     * @dev Reverts if the sender is not approved.
-     * @dev Reverts if `tokenId` is not owned by `from`.
-     * @dev Emits an {IERC721-Transfer} event to address zero.
-     * @param from Current token owner.
-     * @param tokenId Identifier of the token to burn.
-     */
+    /// @notice Burns a token.
+    /// @dev Reverts if the sender is not approved.
+    /// @dev Reverts if `tokenId` is not owned by `from`.
+    /// @dev Emits an {IERC721-Transfer} event to address zero.
+    /// @param from Current token owner.
+    /// @param tokenId Identifier of the token to burn.
     function burnFrom(address from, uint256 tokenId) external;
 
-    /**
-     * Burns a batch of tokens.
-     * @dev Reverts if the sender is not approved for any of `tokenIds`.
-     * @dev Reverts if one of `tokenIds` is not owned by `from`.
-     * @dev Emits an {IERC721-Transfer} event to address zero for each of `tokenIds`.
-     * @param from Current tokens owner.
-     * @param tokenIds Identifiers of the tokens to burn.
-     */
+    /// @notice Burns a batch of tokens.
+    /// @dev Reverts if the sender is not approved for any of `tokenIds`.
+    /// @dev Reverts if one of `tokenIds` is not owned by `from`.
+    /// @dev Emits an {IERC721-Transfer} event to address zero for each of `tokenIds`.
+    /// @param from Current tokens owner.
+    /// @param tokenIds Identifiers of the tokens to burn.
     function batchBurnFrom(address from, uint256[] calldata tokenIds) external;
 }
