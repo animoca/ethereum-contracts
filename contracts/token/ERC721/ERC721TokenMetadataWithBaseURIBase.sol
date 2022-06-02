3fe88c1ebeb92aab4b4c584bbf88c6f32309d9ab
gunnar <gunnark@animocabrands.com>
2022-06-02 12:06:13 +0800

    Add setBaseMetadataURI method on ERC721TokenMetadataWithBaseURIBase

--- contracts/token/ERC721/ERC721TokenMetadataWithBaseURIBase.sol
+++ contracts/token/ERC721/ERC721TokenMetadataWithBaseURIBase.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.14;
 
 import {IERC721Metadata} from "./interfaces/IERC721Metadata.sol";
+import {ContractOwnershipStorage} from "./../../access/libraries/ContractOwnershipStorage.sol";
 import {ERC721Storage} from "./libraries/ERC721Storage.sol";
 import {ERC721TokenMetadataWithBaseURIStorage} from "./libraries/ERC721TokenMetadataWithBaseURIStorage.sol";
 import {ERC721ContractMetadataStorage} from "./libraries/ERC721ContractMetadataStorage.sol";
@@ -15,6 +16,16 @@ abstract contract ERC721TokenMetadataWithBaseURIBase is Context, IERC721Metadata
     using ERC721Storage for ERC721Storage.Layout;
     using ERC721ContractMetadataStorage for ERC721ContractMetadataStorage.Layout;
     using ERC721TokenMetadataWithBaseURIStorage for ERC721TokenMetadataWithBaseURIStorage.Layout;
+     using ContractOwnershipStorage for ContractOwnershipStorage.Layout;
+
+    function setBaseMetadataURI(string calldata baseMetadataURI_) external {
+        ContractOwnershipStorage.layout().enforceIsContractOwner(_msgSender());
+        ERC721TokenMetadataWithBaseURIStorage.layout().setBaseMetadataURI(baseMetadataURI_);
+    }
+
+    function baseMetadataURI() external view returns (string memory) {
+        return ERC721TokenMetadataWithBaseURIStorage.layout().baseMetadataURI();
+    }
 
     /// @inheritdoc IERC721Metadata
     function name() external view override returns (string memory) {
