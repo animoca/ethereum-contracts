577878d1d304215cd438249a0b0ffc31aff318cf
Nathan Sala <sala.nathan@gmail.com>
2022-06-23 04:02:38 +0900

    export ERC1155 test bheavior

--- test/behaviors.js
+++ test/behaviors.js
@@ -1,9 +1,11 @@
 const {supportsInterfaces} = require('./contracts/introspection/behaviors/SupportsInterface.behavior');
 const {behavesLikeERC20} = require('./contracts/token/ERC20/behaviors/ERC20.behavior');
 const {behavesLikeERC721} = require('./contracts/token/ERC721/behaviors/ERC721.behavior');
+const {behavesLikeERC1155} = require('./contracts/token/ERC1155/behaviors/ERC1155.behavior');
 
 module.exports = {
   supportsInterfaces,
   behavesLikeERC20,
   behavesLikeERC721,
+  behavesLikeERC1155,
 };
