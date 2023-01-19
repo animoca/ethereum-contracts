a61ebb50a68676015bdcb1a668efed40712b3894
Nathan Sala <sala.nathan@gmail.com>
2023-01-19 15:21:12 +0900

    Cumulative Merkle Claim (#34)
    * add CumulativeMerkleClaim contract

---
+++ contracts/mocks/payment/CumulativeMerkleClaimMock.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import {CumulativeMerkleClaim} from "./../../payment/CumulativeMerkleClaim.sol";
+
+contract CumulativeMerkleClaimMock is CumulativeMerkleClaim {
+    event Distributed(address recipient, uint256 amount);
+
+    function _distributePayout(address recipient, bytes calldata claimData) internal virtual override {
+        uint256 amount = abi.decode(claimData, (uint256));
+        emit Distributed(recipient, amount);
+    }
+}
