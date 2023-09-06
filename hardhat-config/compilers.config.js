a1ad04945c52f86353685dfbcfc82e1811af3c1f
Nathan Sala <sala.nathan@gmail.com>
2023-09-06 11:39:00 +0900

    target paris evm to prevent generation of PUSH0 opcode

--- hardhat-config/compilers.config.js
+++ hardhat-config/compilers.config.js
@@ -8,6 +8,7 @@ module.exports = {
             enabled: true,
             runs: 99999,
           },
+          evmVersion: 'paris', // until PUSH0 opcode is widely supported
         },
       },
     ],
